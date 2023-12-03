62dd7c349d83380c032ab9f51205e304564a62c5
therasuldev <rasul.ramixanov@gmail.com>
2023-12-03 23:48:50 +0400

    multi hyper links

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:developer';
+
+import 'package:first_flutter_project/animations.dart';
 import 'package:first_flutter_project/lesson/snackbar_without_context.dart';
 import 'package:first_flutter_project/news.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 import 'lesson/pages/index.dart';
@@ -30,22 +34,83 @@ class HomeScaffold extends StatefulWidget {
 }
 
 class _HomeScaffoldState extends State<HomeScaffold> {
-  final pages = [const First(), const Second(), const Third()];
-  final PageController pageController = PageController();
+  //final selectedTexts = ['aa', 'bb'];
+  final sentence = 'asnac aa bibcic bb iecoeince jk scsmonscsc smcosnvd';
 
-  int currentIndex = 0;
+  List<String> overrideText(String sentence) {
+    List<String> newSentence = [];
+    int defaultIndex = 0;
+    int lastIndex = 0;
 
-  final GlobalKey<ScaffoldState> _key = GlobalKey<ScaffoldState>();
+    for (var index = 0; index < sentence.length; index++) {
+      if (sentence[index] == ' ') {
+        if (index > lastIndex) {
+          newSentence.add(sentence.substring(lastIndex, index).trim());
+          defaultIndex++;
+        }
+        lastIndex = index + 1;
+      } else if (index + 1 == sentence.length) {
+        newSentence.add(sentence.substring(lastIndex, index + 1).trim());
+      }
+    }
 
-  void openDrawer() {
-    _key.currentState!.openEndDrawer();
+    return newSentence;
+  }
+
+  late List<String> words;
+
+  @override
+  void initState() {
+    super.initState();
+    words = overrideText(sentence);
+  }
+
+  TextStyle? createStyle(String word, List<String> selectedWords) {
+    if (selectedWords.contains(word)) {
+      return const TextStyle(color: Colors.blue);
+    }
+    return null;
+  }
+
+  TapGestureRecognizer? createOnTaps(String word, Map<String, String> selectedWords) {
+    if (selectedWords.keys.contains(word)) {
+      return TapGestureRecognizer()
+        ..onTap = () {
+          log(selectedWords[word]!);
+        };
+    }
+
+    return null;
+  }
+
+  String sentenceResult(String word, List<String> newLines) {
+    String sentence = '';
+    if (newLines.contains(word)) {
+      sentence += '\n$word ';
+    } else {
+      sentence += '$word ';
+    }
+    return sentence;
   }
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
-        body: News(),
+        body: Center(
+          child: Text.rich(
+            TextSpan(
+              children: List.generate(
+                words.length,
+                (index) => TextSpan(
+                  text: sentenceResult(words[index], ['bb', 'smcosnvd']),
+                  style: createStyle(words[index], ['aa', 'jk', 'jj', 'smcosnvd']),
+                  recognizer: createOnTaps(words[index], {'aa': 'www.aa.com'}),
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
